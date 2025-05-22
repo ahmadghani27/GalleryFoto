92bc600365078d4c05f06d7f43ff6f87c65201e7
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-05-22 20:54:07 +0700

    #1 MEnambahkan code halaman utama file photo/index..

--- GaleriFoto/resources/views/photo/index.blade.php
+++ GaleriFoto/resources/views/photo/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <div class="flex flex-col gap-4 px-8">
-    <div class="sticky top-0 z-40 bg-gray-100 px-[5%] py-4">
+    <header class="sticky top-0 z-40 bg-gray-100 px-[5%] py-4">
         <div class="w-full h-16 px-5 bg-white rounded-[999px] outline outline-1 outline-offset-[-1px] outline-black/10 flex justify-between items-center">
             <div class="flex justify-start items-center gap-4 w-full mr-3.5">
                 <div class="w-6 h-6 bg-zinc-300"></div>
@@ -14,8 +14,8 @@
             </div>
             <div class="w-3.5 h-3.5 bg-zinc-900"></div>
         </div>
-        <!-- Bagian Filter dan Informasi -->
-        <div class="w-full flex justify-between items-center mt-3">
+
+        <article class="w-full flex justify-between items-center mt-3">
             <div class="text-black text-xl font-normal font-inter">Menampilkan 1 Foto</div>
             <div class="flex justify-end items-center gap-5">
                 <div class="h-14 px-5 py-5 bg-white rounded-2xl outline outline-1 outline-offset-[-1px] outline-black/10 flex justify-start items-center gap-2">
@@ -26,13 +26,13 @@
                     <div class="w-3.5 h-3.5 bg-white"></div>
                 </div>
             </div>
-        </div>
-    </div>
+        </article>
+    </header>
 
     <div>
         <div class="self-stretch justify-start text-black text-xl font-medium f-inter">“Monyet”</div>
     </div>
-    <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-3 justify-items-start max-w-full md:justify-items-stretch">
+    <section class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-3 justify-items-start max-w-full md:justify-items-stretch">
         <div class="aspect-square rounded-md overflow-hidden">
             <img src="{{ asset('sample-photo.jpg') }}" alt="Photo" class="w-full h-full object-cover min-w-[240px] max-w-[400px]">
         </div>
@@ -51,6 +51,6 @@
         <div class="aspect-square rounded-md overflow-hidden">
             <img src="{{ asset('sample-photo.jpg') }}" alt="Photo" class="w-full h-full object-cover min-w-[240px] max-w-[400px]">
         </div>
-    </div>
+    </section>
 </div>
 @endsection
