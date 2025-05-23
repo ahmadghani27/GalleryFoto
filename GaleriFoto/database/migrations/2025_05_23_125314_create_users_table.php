2669f75f27780fc5bc166e9fcdc1a1f5bf949bb1
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-05-23 20:10:03 +0700

    ke-4 Merapikan migrasi database

--- GaleriFoto/database/migrations/2025_05_23_125314_create_users_table.php
+++ GaleriFoto/database/migrations/2025_05_23_125314_create_users_table.php
@@ -11,14 +11,32 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('username')->unique();
-            $table->string('email')->unique();
+            // $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
         });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('username')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
