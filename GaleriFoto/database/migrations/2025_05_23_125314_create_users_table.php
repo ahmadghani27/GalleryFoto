cf9403cf1a3f903401197ec32ec6978ca252fbe1
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-05-23 19:58:48 +0700

    #3 (db) Memperbaiki file migrasi database

---
+++ GaleriFoto/database/migrations/2025_05_23_125314_create_users_table.php
@@ -0,0 +1,24 @@
+<?php
+// database/migrations/2025_05_23_194600_create_users_table.php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
