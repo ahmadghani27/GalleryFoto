11fd1a92cd5d843a283f40308a6b5f0af7829c25
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-05-23 20:39:47 +0700

    #7 Akhirnya bisa Memberbaiki databse migrasi

--- GaleriFoto/app/Http/Controllers/Auth/RegisteredUserController.php
+++ GaleriFoto/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -29,11 +29,13 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'username' => $request->username,
+            'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
