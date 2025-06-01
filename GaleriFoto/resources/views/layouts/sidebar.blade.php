1f3eceea7929ae347ce427261d8b3690d9aa9d0a
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-06-01 20:37:16 +0700

    Memperbagus tampilan sidebar

--- GaleriFoto/resources/views/layouts/sidebar.blade.php
+++ GaleriFoto/resources/views/layouts/sidebar.blade.php
@@ -15,16 +15,16 @@
 <body class="antialiased font-inter">
     <div class="flex h-dvh w-full">
         <div
-            x-data="{ open: true }"
-            x-init="open = window.innerWidth >= 768"
-            @resize.window="open = window.innerWidth >= 900"
+            x-data="{ open: localStorage.getItem('sidebarOpen') ? JSON.parse(localStorage.getItem('sidebarOpen')) : window.innerWidth >= 768 }"
+            x-init="open = localStorage.getItem('sidebarOpen') ? JSON.parse(localStorage.getItem('sidebarOpen')) : window.innerWidth >= 768; $watch('open', value => localStorage.setItem('sidebarOpen', value))"
+            @resize.window="open = localStorage.getItem('sidebarOpen') ? JSON.parse(localStorage.getItem('sidebarOpen')) : window.innerWidth >= 900"
             class="flex h-full transition-all duration-300"
             :class="open ? 'w-56' : 'w-16'">
 
             <aside
                 :class="open ? 'w-56' : 'w-16'"
                 class="bg-white text-black fixed top-0 transition-all ease-in-out flex flex-col border-r-[1.5px] border-gray-200 h-dvh">
-                <div class="flex items-center justify-between p-4">
+                <div class="flex items-end justify-between p-4">
                     <span class="text-3xl font-bold"
                         x-transition:enter="transition ease-out duration-300"
                         x-transition:enter-start="opacity-0 -translate-x-10"
@@ -35,42 +35,42 @@
                         x-show="open"><img src="{{ asset('assets/img/Pixelora.png') }}" class="h-8 w-auto" alt="Pixelora">
                     </span>
                     <button @click="open = !open" class="flex items-center transition-all ease-in-out rounded">
-                        <span class="material-symbols-outlined text-[32px]">left_panel_close</span>
+                        <span class="material-symbols-outlined text-[32px] text-zinc-400 translate-y-[2px]">left_panel_close</span>
                     </button>
                 </div>
                 <nav class="p-2 flex">
                     <div class="flex-col w-full">
-                        <div class="text-[14px] text-slate-400 mb-2 px-2 font-semibold" x-show="open">MENU</div>
+                        <div class="text-[14px] text-black/50 mb-2 px-2 font-semibold " x-show="open">MENU</div>
                         <div class="w-full h-[1.5px] bg-slate-200 mb-2" x-show="!open"></div>
-                        <a href="{{ route('foto') }}" class="flex items-center gap-4 px-2 py-4 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('foto*') ? '!bg-slate-100 icon-filled' : '' }}">
-                            <span class="material-symbols-outlined text-[32px]">image</span>
-                            <span class="text-[20px] font-normal" x-show="open">Foto</span>
+                        <a href="{{ route('foto') }}" class="flex items-center gap-4 px-2 py-4 my-1  w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('foto*') ? '!bg-cyan-600' : '' }}">
+                            <span class="material-symbols-outlined text-[32px] {{ Route::is('foto*') ? '!text-white' : '' }}">image</span>
+                            <span class="text-[20px] font-normal {{ Route::is('foto*') ? '!text-white' : '' }}" x-show="open">Foto</span>
                         </a>
-                        <a href="{{ route('album') }}" class="flex items-center gap-4 px-2 py-4 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('album*') ? 'bg-cyan-600' : '' }}">
-                            <span class="material-symbols-outlined text-[32px] {{ Route::is('album*') ? 'text-white' : '' }}" >folder_open</span>
-                            <span class="text-[20px] font-normal {{ Route::is('album*') ? 'text-white' : '' }}" x-show="open">Album</span>
+                        <a href="{{ route('album') }}" class="flex items-center gap-4 px-2 py-4 my-1 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('album*') ? '!bg-cyan-600' : '' }}">
+                            <span class="material-symbols-outlined text-[32px] {{ Route::is('album*') ? '!text-white' : '' }}">folder_open</span>
+                            <span class="text-[20px] font-normal {{ Route::is('album*') ? '!text-white' : '' }}" x-show="open">Album</span>
                         </a>
-                        <a href="{{ route('arsip') }}" class="flex items-center gap-4 px-2 py-4 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('arsip*') ? '!bg-slate-100 icon-filled' : '' }}">
-                            <span class="material-symbols-outlined text-[32px]">archive</span>
-                            <span class="text-[20px] font-normal" x-show="open">Arsip</span>
+                        <a href="{{ route('arsip') }}" class="flex items-center gap-4 px-2 py-4 my-1 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('arsip*') ? '!bg-cyan-600' : '' }}">
+                            <span class="material-symbols-outlined text-[32px] {{ Route::is('arsip*') ? '!text-white' : '' }}">archive</span>
+                            <span class="text-[20px] font-normal {{ Route::is('arsip*') ? '!text-white' : '' }}" x-show="open">Arsip</span>
                         </a>
-                        <a href="{{ route('favorit') }}" class="flex items-center gap-4 px-2 py-4 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('favorit*') ? '!bg-slate-100 icon-filled' : '' }}">
-                            <span class="material-symbols-outlined text-[32px]">favorite</span>
-                            <span class="text-[20px] font-normal" x-show="open">Favorit</span>
+                        <a href="{{ route('favorit') }}" class="flex items-center gap-4 px-2 py-4 my-1 w-full rounded-2xl bg-white hover:bg-slate-100 {{ Route::is('favorit*') ? '!bg-cyan-600' : '' }}">
+                            <span class="material-symbols-outlined text-[32px] {{ Route::is('favorit*') ? '!text-white' : '' }}">favorite</span>
+                            <span class="text-[20px] font-normal {{ Route::is('favorit*') ? '!text-white' : '' }}" x-show="open">Favorit</span>
                         </a>
-                        <div class="text-[14px] text-slate-400 mb-2 mt-4 px-2 font-semibold" x-show="open">ANDA</div>
+                        <div class="text-[14px] text-black/50 mb-2 mt-4 px-2 font-semibold gap-1" x-show="open">ANDA</div>
                         <div class="w-full h-[1.5px] bg-slate-200 mb-2" x-show="!open"></div>
-                        <a href="{{ route('akun') }}" class="flex items-center gap-4 px-2 py-4 w-full rounded-2xl bg-white hover:bg-slate-100 transition-all ease-in-out {{ Route::is('akun') ? '!bg-slate-100 icon-filled' : '' }}">
-                            <span class="material-symbols-outlined text-[32px]">account_box</span>
-                            <span class="text-[20px] font-normal" x-show="open">Akun</span>
+                        <a href="{{ route('akun') }}" class="flex items-center gap-4 px-2 py-4 my-1 w-full rounded-2xl bg-white hover:bg-slate-100 transition-all ease-in-out {{ Route::is('akun') ? '!bg-cyan-600' : '' }}">
+                            <span class="material-symbols-outlined text-[32px] {{ Route::is('akun*') ? '!text-white' : '' }}">account_box</span>
+                            <span class="text-[20px] font-normal {{ Route::is('akun*') ? '!text-white' : '' }}" x-show="open">Akun</span>
                         </a>
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
 
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="flex items-center gap-4 px-2 py-4 w-full rounded-2xl bg-white hover:bg-slate-100">
-                                <span class="material-symbols-outlined text-[32px]">exit_to_app</span>
-                                <span class="text-[20px] font-normal" x-show="open">Keluar</span>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="flex items-center gap-4 px-2 py-4 my-1 w-full rounded-2xl bg-white hover:bg-slate-100">
+                                <span class="material-symbols-outlined text-[32px] {{ Route::is('logout*') ? '!text-white' : '' }}">exit_to_app</span>
+                                <span class="text-[20px] font-normal {{ Route::is('logout*') ? '!text-white' : '' }}" x-show="open">Keluar</span>
                             </a>
                         </form>
                     </div>
