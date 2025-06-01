6ad426de9fae72f3c2d91572b592d8b16da50f99
Yoshi Kinjang <audewaputro15@gmail.com>
2025-06-01 17:28:27 +0700

    detail foto, masih halaman foto dan favorit

--- GaleriFoto/routes/web.php
+++ GaleriFoto/routes/web.php
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/foto/single-pindahalbum', [PhotoController::class, 'pindahAlbum'])->name('foto.singlepindahalbum');
     Route::patch('/foto/favorite', [PhotoController::class, 'toggleFavorite'])->name('foto.togglefavorite');
     Route::get('/api/getActiveAlbum/{id}', [AlbumController::class, 'getActiveAlbum'])->name('api.getActiveAlbum');
-    Route::get('/api/getAllActiveAlbum', [AlbumController::class, 'getAllActiveAlbum'])->name('api.getAllActiveAlbum');
+    Route::get('/api/getAllActiveAlbum', [AlbumController::class,  'getAllActiveAlbum'])->name('api.getAllActiveAlbum');
     Route::get('/foto_access/{path}', [PhotoController::class, 'access'])
         ->where('path', '.*')
         ->name('foto.access');
@@ -44,6 +44,14 @@ Route::middleware('auth')->group(function () {
     Route::patch('/foto/multiple-delete', [PhotoController::class, 'massDestroy'])->name('foto.multipledelete');
     Route::patch('/foto/multiple-arsip', [PhotoController::class, 'massArsipkan'])->name('foto.multiplearsip');
     Route::patch('/foto/multipleunarsip', [PhotoController::class, 'massUnarsipkan'])->name('foto.multipleunarsip');
+    Route::get('api/foto/{id}', [PhotoController::class, 'api_get_detail_global_foto'])->name('foto.detail');
+    
+    Route::patch('/raw-arsip', [PhotoController::class, 'arsipkanRaw'])->name('arsipkanRaw');
+    Route::delete('/raw-delete', [PhotoController::class, 'deleteRaw'])->name('deleteRaw');
+    Route::get('/download-file/{path}', [PhotoController::class, 'downloadFoto'])
+        ->where('path', '.*')
+        ->name('downloadFile');
+
     //Route Profile
     Route::get('/akun', [ProfileController::class, 'edit'])->name('akun');
     Route::patch('/akun/profile', [ProfileController::class, 'update'])->name('profile.update');
