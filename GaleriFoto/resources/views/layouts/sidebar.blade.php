2c9f12f579dc3fc8b268de45bb6b9cbdab8f193a
Ahmadghani27 <ghoniyusufghoniyusuf@gmail.com>
2025-06-01 17:49:51 +0700

    Menambah logo

--- GaleriFoto/resources/views/layouts/sidebar.blade.php
+++ GaleriFoto/resources/views/layouts/sidebar.blade.php
@@ -8,6 +8,7 @@
 
     <title>@yield('title', 'Pixelora')</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('storage/icon.ico') }}" type="image/x-icon">
 
     @vite(['resources/css/styles.css', 'resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -20,11 +21,10 @@
             @resize.window="open = window.innerWidth >= 900"
             class="flex h-full transition-all duration-300"
             :class="open ? 'w-56' : 'w-16'">
-            
-            <aside 
+
+            <aside
                 :class="open ? 'w-56' : 'w-16'"
-                class="bg-white text-black fixed top-0 transition-all ease-in-out flex flex-col border-r-[1.5px] border-gray-200 h-dvh"
-            >
+                class="bg-white text-black fixed top-0 transition-all ease-in-out flex flex-col border-r-[1.5px] border-gray-200 h-dvh">
                 <div class="flex items-center justify-between p-4">
                     <span class="text-3xl font-bold"
                         x-transition:enter="transition ease-out duration-300"
@@ -33,14 +33,15 @@
                         x-transition:leave="transition ease-in duration-300"
                         x-transition:leave-start="opacity-100 translate-x-0"
                         x-transition:leave-end="opacity-0 -translate-x-10"
-                        x-show="open">Pixelora</span>
-                    <button @click="open = !open" class="flex  transition-all ease-in-out rounded">
+                        x-show="open"><img src="{{ asset('storage/Pixelora.png') }}" class="h-8 w-auto" alt="Pixelora">
+                    </span>
+                    <button @click="open = !open" class="flex items-center transition-all ease-in-out rounded">
                         <span class="material-symbols-outlined text-[32px]">left_panel_close</span>
                     </button>
                 </div>
                 <nav class="p-2 flex">
                     <div class="flex-col w-full">
-                        <div class="text-[14px] text-slate-400 mb-2 px-2 font-semibold"  x-show="open">MENU</div>
+                        <div class="text-[14px] text-slate-400 mb-2 px-2 font-semibold" x-show="open">MENU</div>
                         <div class="w-full h-[1.5px] bg-slate-200 mb-2" x-show="!open"></div>
                         <a href="{{ route('foto') }}" class="flex items-center gap-4 px-2 py-4 w-full rounded-md bg-white hover:bg-slate-100 {{ Route::is('foto*') ? '!bg-slate-100 icon-filled' : '' }}">
                             <span class="material-symbols-outlined text-[32px]">image</span>
